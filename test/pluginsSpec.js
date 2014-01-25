01b9374c593fa4948833f8ec3e92519ab02b048b
Carl Ripa <cj.ripa@gmail.com>
2014-01-25 21:19:30 +0100

    Added tests for plugins.js

---
+++ test/pluginsSpec.js
@@ -0,0 +1,50 @@
+describe('plugins', function(){
+    var _plugins;
+
+    beforeEach(function(){
+        _plugins = undefined;
+        _plugins = require('../src/plugins');
+    });
+
+    describe('#watch()', function(){
+        it('should polyfill Object.watch', function(){
+            assert.isDefined(Object.watch, 'it is defined');
+        });
+        it('should trigger when a property changes on an object', function(){
+            var obj = {
+                val: 1
+            };
+
+            assert.throw(function(){
+                obj.watch('val', function(id, oldVal, newVal) {
+                    if(oldVal !== newVal){
+                        throw id + ': ' + oldVal + ' -> ' + newVal;
+                    }
+                });
+                obj.val = 2;
+            }, 'val: 1 -> 2');
+
+        });
+    });
+
+    describe('#unwatch()', function(){
+        it('should polyfill Object.unwatch', function(){
+            assert.isDefined(Object.unwatch, 'it is defined');
+        });
+
+        it('should unbind the watch of a property', function(){
+            var obj = {
+                val: 1
+            };
+
+            assert.doesNotThrow(function(){
+                var fn = function(id, oldVal, newVal) {
+                    throw 'value changed';
+                };
+                obj.watch('val', fn);
+                obj.unwatch('val');
+                obj.val = 2;
+            }, 'did not trigger watch');
+        });
+    });
+});
